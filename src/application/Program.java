3502fc2d3decac70df54a111b9395330a3802327
Weslin-0101 <70270543+Weslin-0101@users.noreply.github.com>
2021-06-06 14:47:45 -0300

    Melhorando SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,8 +13,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); // Testando a instanciação
 		
-		Seller seller = sellerDao.findById(3);
-		
+		System.out.println("=== TEST 1: seller findById ===");
+		Seller seller = sellerDao.findById(3); // testando a classe
 		System.out.println(seller);
 	}
 }
